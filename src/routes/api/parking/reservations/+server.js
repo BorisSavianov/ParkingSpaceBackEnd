07c9541556460f5f6f1f89592799ff3206cb4167
BorisSavianov <bsavianov@gmail.com>
2025-07-06 10:38:31 +0300

    test cors

--- src/routes/api/parking/reservations/+server.js
+++ src/routes/api/parking/reservations/+server.js
@@ -265,8 +265,8 @@ export async function OPTIONS() {
   return new Response(null, {
     headers: {
       "Access-Control-Allow-Origin": "*", // Specify the url you wish to permit
-      "Access-Control-Allow-Methods": "POST, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "POST, OPTIONS, GET",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
     },
   });
 }
